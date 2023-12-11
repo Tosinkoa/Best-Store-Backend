af3d4a549b1c20b45553a20a3095995bc6f550bd
Paul Ojo <90416105+Tosinkoa@users.noreply.github.com>
2023-12-11 06:32:11 +0100

    Removed otpauth

--- src/api/Admin-APIs/Authentication/AuthenticationEndpoints.js
+++ src/api/Admin-APIs/Authentication/AuthenticationEndpoints.js
@@ -41,60 +41,6 @@ router.post("/admin/login", async (req, res) => {
   });
 });
 
-// router.get("/admin/otp-setup", AdminAuthMiddleware, async (req, res) => {
-//   const loggedInAdmin = req.session.user;
-//   try {
-//     // Check if user already have a valid
-//     const adminSecret = await AuthenticationQueries.selectLoggedInAdminScerets([
-//       loggedInAdmin,
-//     ]);
-//     if (adminSecret?.rows[0]?.temp_secret || adminSecret?.rows[0]?.valid_secret) {
-//       // Al / Ve ==> Already verified
-//       return res.status(400).json({
-//         error:
-//           "Al / Ve, Something went wrong, if you're seeing this error, kindly reach out to the support.",
-//       });
-//     }
-//     // Generate a new TOTP object
-//     const totp = new TOTP({
-//       issuer: process.env.NODE_ENV === "production" ? "Best Store" : "Best Store Dev",
-//       label: "Admin",
-//       algorithm: "SHA1",
-//       digits: 6,
-//       period: 30,
-//       secret: new Secret(),
-//     });
-
-//     // Get the TOTP URI
-//     const totpUri = totp.toString();
-
-//     // Generate QR Code
-//     QRCode.toDataURL(totpUri, async (err, data_url) => {
-//       if (err) {
-//         return res.status(400).json({ error: "An error occurred, please try again!" });
-//       }
-
-//       const isAdminExist = await AuthenticationQueries.selectAdmin([loggedInAdmin]);
-
-//       // Check if admin temp_secret exist, then store the secret in the database
-//       const validScret = null;
-//       if (isAdminExist.rowCount > 0) {
-//         await AuthenticationQueries.updateAdminAuthSecret([
-//           totp.secret.base32,
-//           validScret,
-//           loggedInAdmin,
-//         ]);
-//       } else {
-//         await AuthenticationQueries.insertAdminAuthSecret([totp.secret.base32, loggedInAdmin]);
-//       }
-//       return res.status(200).json({ data: { qr_code: data_url, token: totp.secret.base32 } });
-//     });
-//   } catch (e) {
-//     console.error(e);
-//     return res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-
 // First time verification for adding temp secret to valid secret row in the database
 router.get("/admin/otp-setup", AdminAuthMiddleware, async (req, res) => {
   const loggedInAdmin = req.session.user;
@@ -151,70 +97,6 @@ router.get("/admin/otp-setup", AdminAuthMiddleware, async (req, res) => {
     return res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
-// router.post("/admin/first-time-verify-otp", AdminAuthMiddleware, async (req, res) => {
-//   const { user_token } = req.body;
-//   const loggedInAdmin = req.session.user;
-
-//   if (!user_token) {
-//     // No / To ==> No Token
-//     return res.status(400).json({
-//       error:
-//         "No / To, Something went wrong, if you're seeing this error, kindly reach out to the support.",
-//     });
-//   }
-
-//   try {
-//     // Retrieve the stored secret from the database
-//     const adminSecret = await AuthenticationQueries.selectLoggedInAdminScerets([
-//       loggedInAdmin,
-//     ]);
-
-//     // No / Se / Ot ==> Not setup otp
-//     if (adminSecret.rowCount < 1) {
-//       return res.status(400).json({
-//         error:
-//           "No / Se / Ot, Something went wrong, if you're seeing this error, kindly reach out to the support.",
-//       });
-//     }
-//     // If there is no temporary secret, this means admin has made a first time verification
-//     const adminTempSecret = adminSecret.rows[0].temp_secret;
-//     if (!adminTempSecret) {
-//       return res.status(400).json({ error: "Duplicate verification detected!" });
-//     }
-
-//     const totp = new TOTP({
-//       secret: adminTempSecret,
-//       algorithm: "SHA1",
-//       digits: 6,
-//       period: 30,
-//     });
-
-//     const isValidToken = totp.validate({
-//       token: user_token,
-//       // window: undefined,
-//     });
-
-//     if (!isValidToken) {
-//       return res.status(400).json({ error: "The code you entered is incorrect!" });
-//     }
-
-//     const newTempSecret = null;
-//     const verified = true;
-//     // Change temp_secret to valid secret in the database
-//     await AuthenticationQueries.updateAllAdminSecrets([
-//       newTempSecret,
-//       verified,
-//       adminSecret.rows[0].temp_secret,
-//       loggedInAdmin,
-//     ]);
-
-//     return res.status(200).json({ data: "Successfully verified" });
-//   } catch (e) {
-//     console.error(e);
-//     return res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
 
 router.post("/admin/first-time-verify-otp", AdminAuthMiddleware, async (req, res) => {
   const { user_token } = req.body;
@@ -276,63 +158,6 @@ router.post("/admin/first-time-verify-otp", AdminAuthMiddleware, async (req, res
     return res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
-// This check if the user auth is valid using the valid_secret
-// router.post("/admin/verify-otp", AdminAuthMiddleware, async (req, res) => {
-//   const { user_token } = req.body;
-//   const loggedInAdmin = req.session.user;
-
-//   if (!user_token) {
-//     // No / To ==> No Token
-//     return res.status(400).json({
-//       error:
-//         "No / To, Something went wrong, if you're seeing this error, kindly reach out to the support.",
-//     });
-//   }
-
-//   try {
-//     // Retrieve the stored secret from the database
-//     const adminSecret = await AuthenticationQueries.selectLoggedInAdminScerets([
-//       loggedInAdmin,
-//     ]);
-//     // No / Se / Ot ==> Not setup otp
-//     if (adminSecret.rowCount < 1) {
-//       return res.status(400).json({
-//         error:
-//           "No / Se / Ot, Something went wrong, if you're seeing this error, kindly reach out to the support.",
-//       });
-//     }
-//     const adminValidSecret = adminSecret.rows[0].valid_secret;
-//     // If there is no valid secret, this means admin is yet to do a first time verification
-//     if (!adminValidSecret) {
-//       return res.status(400).json({ error: "First time verification is required!" });
-//     }
-
-//     const totp = new TOTP({
-//       secret: adminValidSecret,
-//       algorithm: "SHA1",
-//       digits: 6,
-//       period: 30,
-//     });
-
-//     const isValidToken = totp.validate({
-//       token: user_token,
-//       window: 1,
-//     });
-
-//     if (!isValidToken) {
-//       return res.status(400).json({ error: "The code you entered is incorrect!" });
-//     }
-
-//     const verified = true;
-//     await AuthenticationQueries.updateAdminOtpVerified([verified, loggedInAdmin]);
-
-//     return res.status(200).json({ data: "Successfully verified" });
-//   } catch (e) {
-//     console.error(e);
-//     return res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
 
 // This check if the user auth is valid using the valid_secret
 router.post("/admin/verify-otp", AdminAuthMiddleware, async (req, res) => {
